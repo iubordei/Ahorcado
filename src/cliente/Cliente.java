b8eb0569a2a52808e93f1b1211cddc05fd43ae22
alescalo <alescalo@unirioja.es>
2020-12-30 18:11:02 +0100

    Cliente casi heche e introduccion al servidor.
    El cliente esta casi finalizado, su funcionamiento se basa en lo que le
    envia el servidor. Por tanto el servidor tiene que enviar la informacion
    al cliente en un orden (ver funcionamiento del cliente).
    Los comandos se usan para saber que tiene que hacer tanto el servidor
    como el cliente.

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -1,33 +1,154 @@
 package cliente;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Cliente {
-	private String nombre;
-	private int idPartida;
-	private Socket socket;
 
-	public Cliente(String nombre, Socket socket) {
+	// Constants
+	private static final int COMANDO_MOSTRAR_MENU = 0;
+	private static final int COMANDO_MOSTAR_PARTIDAS = 1;
+	private static final int COMANDO_TU_TURNO = 2;
+	private static final int COMANDO_ACTULIZACION_PARTIDA = 3;
+	private static final int COMANDO_CREAR_PARTIDA = 4;
+	private static final int COMANDO_UNIRSE_PARTIDA = 5;
+	private static final int COMANDO_SALIR = 6;
+	private static final int COMANDO_INTRODUCIR_LETRA = 7;
+	private static final int COMANDO_INTRODUCIR_PALABRA = 8;
+	
+	private static final String TEXTO_MENU = ""
+			+ "Bienvenido al ahoracado %s\r\n" 
+			+ "¿Qué deseas hacer?\r\n\r\n"
+			+ "  - 1. Crear partida\r\n" 
+			+ "  - 2. Unirse a una partida\r\n" 
+			+ "  - 3. Salir\r\n";
+	private static final String TEXTO_TURNO = ""
+			+ "Es tu turno, ¿qué deseas hacer?"
+			+ "  - 1. Introducir una letra\r\n"
+			+ "  - 2. Resolver la palabra\r\n";
+	private static final String TEXTO_INTRODUCIR_NOMBRE = "Introduce tu nombre:";
+	private static final String TEXTO_NO_HAY_PARTIDAS_ACTIVAS = ""
+			+ "No hay partidas activas. ¿Quieres crear una?\r\n"
+			+ "  - 1. Sí\r\n"
+			+ "  - 2. No\r\n";
+	
+	// Variables.
+	private int idCliente; // atributo para asociar un cliente a un jugador.
+	private int idPartida; // atributo para unir un cliente a una partida.
+	
+	private String nombre;
+	
+	private Socket socket;
+	
+	private DataInputStream in;
+	private DataOutputStream out;
+
+	public Cliente(String nombre, Socket socket) throws IOException {
+		this.idCliente = (int) System.currentTimeMillis();
 		this.nombre = nombre;
 		this.socket = socket;
+		in = new DataInputStream(socket.getInputStream()); // Leemos del servidor
+		out = new DataOutputStream(socket.getOutputStream()); // Escribimos hacia el servidor
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Introduce el nombre de tu jugador");
+		System.out.println(TEXTO_INTRODUCIR_NOMBRE);
 		Scanner scanner = new Scanner(System.in);
 		String nombre = scanner.nextLine();
 		scanner.close();
-		try {
-			Socket socket = new Socket("localhost", 10000);
-			Cliente cliente = new Cliente(nombre, socket);
 
+		try (Socket server = new Socket("localhost", 10000);) // Establecemos la conexion con el servidor.
+		{
+			Cliente cliente = new Cliente(nombre, server);
+			cliente.waitForData();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public void waitForData() throws IOException {
+		int command = -1;
+		while ((command = in.read()) != -1) {
+			switch (command) {
+			case COMANDO_MOSTRAR_MENU:
+				mostrarMenu();
+				break;
+			case COMANDO_MOSTAR_PARTIDAS:
+				unirsePartida();
+				break;
+			case COMANDO_TU_TURNO:
+				jugarTurno();
+				break;
+			case COMANDO_ACTULIZACION_PARTIDA:
+				actualizarPartida();
+				break;
+			}
+		}
+	}
+
+	// Este metodo muestra el menu de opciones iniciales del cliente y retorna la
+	// opcion escogida por el cliente.
+	private void mostrarMenu() throws IOException {
+		// Enseñar el menu y leer la opción que elige el cliente.
+		int opcion = -1;
+		while (opcion < 1 || opcion > 3) {
+			System.out.println(String.format(TEXTO_MENU, nombre));
+
+			Scanner scanner = new Scanner(System.in);
+			opcion = scanner.nextInt();
+			scanner.close();
+		}
+		// Retornar la opción elegida.
+		switch (opcion) {
+		case 1:
+			opcion = COMANDO_CREAR_PARTIDA;
+		case 2:
+			opcion = COMANDO_UNIRSE_PARTIDA;
+		case 3:
+			opcion = COMANDO_SALIR;
+		}
+		out.write(opcion);
+	}
+
+	// Este metodo muestra todas las partidas activas y retorna un entero que indica
+	// la partida a la que quiere unirse el cliente.
+	private void unirsePartida() throws IOException {
+		// Enseñar todas las partidas.
+		int numPartidas = 0;
+		numPartidas = in.read();
+		int opcion = -1;
+		if (numPartidas == 0) { //No hay partidas activas.
+			System.out.print(TEXTO_NO_HAY_PARTIDAS_ACTIVAS);
+			Scanner scanner = new Scanner(System.in);
+			opcion = scanner.nextInt();
+			scanner.close();
+			switch (opcion) {
+			case 1:
+				out.write(COMANDO_CREAR_PARTIDA); // si quieres crear partida le envias al servidor un 1 indicando que quieres crear una partida.
+			default:
+				mostrarMenu(); // si no quieres crear partida, vuelves al menu inicial.
+			}
+			return;
+		}
+		String linea = null;
+		while ((linea = in.readLine()) != null) {
+			System.out.println(linea);
+		}
+		// Leer el número de partida que elige el cliente.
+		while (opcion < 1 || opcion > numPartidas) {
+			Scanner scanner = new Scanner(System.in);
+			opcion = scanner.nextInt();
+			scanner.close();
+		}
+		out.write(COMANDO_UNIRSE_PARTIDA);
+		out.write(opcion);
+	}
+
+	// Este metodo obtiene la letra(String) que el cliente ha introducido por
+	// teclado para posteriormente enviarla a través del Socket.
 	public String introducirLetra() {
 		System.out.println("Introduce la letra");
 		Scanner scanner = new Scanner(System.in);
@@ -36,7 +157,9 @@ public class Cliente {
 		return letra;
 	}
 
-	public String introducirSolucion() {
+	// Este metodo obtiene la palabra(String) que el cliente ha introducido por
+	// teclado para posteriormente enviarla a través del Socket.
+	public String introducirPalabra() {
 		System.out.println("Introduce la palabra");
 		Scanner scanner = new Scanner(System.in);
 		String palabra = scanner.nextLine();
@@ -44,15 +167,50 @@ public class Cliente {
 		return palabra;
 	}
 
-	public String toString() {
-		return "Nombre: " + nombre + ", partida: " + idPartida;
+	public void jugarTurno() throws IOException {
+		int opcion = -1;
+		while (opcion < 1 || opcion > 2) {
+			System.out.println(TEXTO_TURNO);
+			Scanner scanner = new Scanner(System.in);
+			opcion = scanner.nextInt();
+			scanner.close();
+		}
+		switch (opcion) {
+		case 1:
+			String letra = introducirLetra();
+			out.write(COMANDO_INTRODUCIR_LETRA);
+			out.writeBytes(letra);
+		case 2:
+			String palabra = introducirPalabra();
+			out.write(COMANDO_INTRODUCIR_PALABRA);
+			out.writeBytes(palabra);
+		}
+	}
+	
+	public void actualizarPartida() throws IOException {
+		String linea = null;
+		while ((linea = in.readLine()) != null) {
+			System.out.println(linea);
+		}
+	}
+	// Este metodo obtiene el id de un cliente para poder relacionar lo que hace
+	// este cliente con el correspondiente jugdor.
+	public int getIdCliente() {
+		return idCliente;
 	}
 
-	// muestra todas las partidas y las 2 opciones disponibles para el cliente
-	// crear partida:
-	// unirse a partida:
-	public void menu() {
+	// Este metodo obtiene el nombre del cliente.
+	public String getNombre() {
+		return nombre;
+	}
 
+	// Este metodo obtiene el idPartida de un cliente.
+	public int getIdPartida() {
+		return idPartida;
+	}
+
+	public String toString() {
+		return "Nombre: " + nombre + ", partida: " + idPartida;
 	}
 
 }
--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -1,23 +1,36 @@
 package servidor;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Servidor {
+	// Constants
+	private static final int COMANDO_MOSTRAR_MENU = 0;
+	private static final int COMANDO_MOSTAR_PARTIDAS = 1;
+	private static final int COMANDO_TU_TURNO = 2;
+	private static final int COMANDO_ACTULIZACION_PARTIDA = 3;
+	private static final int COMANDO_CREAR_PARTIDA = 4;
+	private static final int COMANDO_UNIRSE_PARTIDA = 5;
+	private static final int COMANDO_SALIR = 6;
+	private static final int COMANDO_INTRODUCIR_LETRA = 7;
+	private static final int COMANDO_INTRODUCIR_PALABRA = 8;
 
 	private static List<Partida> partidas = new ArrayList<>();
 
 	public static void main(String[] args) {
-		try {
+		int opcionRecibida;
+		try (ServerSocket server = new ServerSocket(10000)) {
 
 			// partidaPrueba sin sockets
 
 			ExecutorService pool = Executors.newCachedThreadPool();
-			ServerSocket server = new ServerSocket(10000);
 			Jugador j1 = new Jugador("paco", null);
 			Jugador j2 = new Jugador("pepe", null);
 
@@ -27,13 +40,45 @@ public class Servidor {
 
 			partidas.add(partida);
 			pool.submit(partida);
-			
+
 			pool.shutdown();
 
 			// Fin partidaPrueba sin sockets
 
-			while (true) {
+			try (Socket cliente = server.accept(); // Establecemos la conexion con 1 cliente.
+					DataInputStream in = new DataInputStream(cliente.getInputStream());
+					DataOutputStream out = new DataOutputStream(cliente.getOutputStream())) {
 
+				out.writeByte(COMANDO_MOSTRAR_MENU); // El servidor escribe al cliente, para que éste muestre el
+														// menu.
+				opcionRecibida = in.read();
+				switch (opcionRecibida) {
+				case COMANDO_CREAR_PARTIDA: // Caso en el que el cliente quiere crear partida.
+
+					/*
+					 * INCOMPLETO
+					 */
+
+					break;
+				case COMANDO_UNIRSE_PARTIDA: // Caso en el que el cliente quiere unirse a una partida.
+					// Enviar al cliente un numero indicando el numero de partidas, seguido de todas
+					// las partidas con saltos de linea.
+					// para que el cliente elija el numero de la partida
+
+					/*
+					 * INCOMPLETO
+					 */
+
+					break;
+				case COMANDO_SALIR: // Caso en el que el cliente quiere salir.
+
+					break;
+				/*
+				 * INCOMPLETO
+				 */
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 
 		} catch (IOException e) {
